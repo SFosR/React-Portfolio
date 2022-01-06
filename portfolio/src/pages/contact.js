8e15fd9550d47813028d9cc9aba0917c56d87ba2
Sheri Foster <fostr.sheri@gmail.com>
2022-01-06 17:59:42 -0500

    "uploaded some pics & started added projects"

--- portfolio/src/pages/contact.js
+++ portfolio/src/pages/contact.js
@@ -3,7 +3,7 @@ import React from "react";
 function Contact() {
     return (
       <div className="contact-info">
-        My Info
+        <p>You can reach me at fostr.sheri@gmail.com</p>
       </div>
     );
   }
--- portfolio/src/pages/portfolio.js
+++ portfolio/src/pages/portfolio.js
@@ -5,17 +5,43 @@ function Portfolio() {
     return (
       <div className="my-portfolio">
         My Portfolio
-        <ProjectCard title="Weather Dashboard" 
-        img="jpeg"
-        description="text for desc goes here" 
-        github="insert github link"
-        livelink="insert live link" />
+        <ProjectCard title="Project Picker" 
+        img="./img/projectpicker.png"
+        description="Project that helps future students find ideas for projects. 
+        Users can share project ideas and search for projects." 
+        github="https://github.com/SFosR/Project-Picker.git"
+        livelink="https://project-picker-project-2.herokuapp.com/" />
 
         <ProjectCard title="Weather Dashboard" 
         img="jpeg"
-        description="text for desc goes here" 
-        github="insert github link"
-        livelink="insert live link" />
+        description="Allows users to pull up any city in the USA and find the current weather. 
+        It will provide current temperature, humidity, UV index and a 5-day forecast." 
+        github="https://github.com/SFosR/Weather-Dash.git"
+        livelink="https://sfosr.github.io/Weather-Dash/" />
+
+        <ProjectCard title="Note Taker" 
+        img="./img/notetaker.png"
+        description="" 
+        github="https://github.com/SFosR/Note-Taker.git"
+        livelink="https://note-taker-shf.herokuapp.com/" />  
+
+        <ProjectCard title="Another Project" 
+        img="jpeg"
+        description="" 
+        github=""
+        livelink="" /> 
+
+        <ProjectCard title="Another Project" 
+        img="jpeg"
+        description="" 
+        github=""
+        livelink="" /> 
+
+        <ProjectCard title="Another Project" 
+        img="jpeg"
+        description="" 
+        github=""
+        livelink="" />    
 
         {/* repeat for each project */}
       </div>
