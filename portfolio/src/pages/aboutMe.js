0560b245ad38ba56bb5bf093cd380451b9952eaa
Sheri Foster <fostr.sheri@gmail.com>
2022-01-06 16:46:43 -0500

    "added about me verbage"

--- portfolio/src/pages/aboutMe.js
+++ portfolio/src/pages/aboutMe.js
@@ -5,7 +5,11 @@ function AboutMe() {
     return (
       <div className="about-me">
         <img src="mypic from my img file"/>
-        text About Me 
+      <p>I always thought I should be an engineer of some sort. In fact, every "career test" I've ever taken has said so. So I decided to actually do something about it.  
+      After years in sales, having success but not completely loving it, I enrolled in to coding bootcamp. I'mFull Stack web developer wiwth skills in CSS, HTML, JavaScript, MySql, React with more to come. Recently earned a certificate in 
+      full stack develpment from THE Ohio State University. With a background in sales, I'm excited to bring my problem solving, independent and 
+      leadership skills to create meaningful web applicatons. I'm experienced in working in a fast-paced environment, working independently and also as a team. I'm a firm believer 
+      that in this ever changing world, you must constantly educate yourself as to the lastest techinques. </p>l
       </div>
     );
   }
