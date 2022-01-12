e28e3061820ff080e25e460d0ef50a39fc65bf74
Sheri Foster <fostr.sheri@gmail.com>
2022-01-12 15:27:00 -0500

    "changed footer"

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,32 +1,13 @@
 import React from 'react';
-// import { Row, Col } from 'react-bootstrap';
-// import { AiFillGitHub } from 'react-icons/ai';
-// import { FaLinkedIn } from "react-icons/fa";
+import { SiLinkedIn, SiGithub } from 'react-icons/si';
 
 function Footer() {
     return (
-      <div className="footer">
-        {Footer /* <Row>
-          <Col md='4'>
-            <p>Created By: Sheri Foster</p>
-          </Col>
-          <Col md='4'>
-            <p>2022</p>
-          </Col>
-          <Col md='4'>
-            <ul className='iconLinks'>
-              <li className='iconLinks'>
-                <a href="https://github.com/SFosR" style={{ color: 'black' }}><AiFillGitHub size='25px' /></a>
-              </li>
-              <li className='iconLinks'>
-                <a href=""  style={{ color: 'black' }}><FaLinkedIn size='25px' /></a>
-              </li>
-            </ul>
-          </Col>
-        </Row> */}
-
+      <div className='icons bg-dark py-3 container-fluid d-flex justify-content-center text-light'>
+          <a href='https://www.linkedin.com/in/fostergroupohio/' target={"_blank"} className='icons'><SiLinkedIn/></a>
+          <a href='https://github.com/SFosR' target={"_blank"} className='icons'><SiGithub/></a>
       </div>
-    );
+    )
   }
   
   export default Footer;
