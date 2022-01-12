22a3c930ec66148d4500f22ff03bd8ef8964010d
Sheri Foster <fostr.sheri@gmail.com>
2022-01-12 15:06:41 -0500

    "changed header"

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Resume from '../Assets/Resume_Sheri Foster.pdf';
 
 
-function Header() {
-    return (
-      <div className="header">
-        <h1>Sheri Foster</h1>
-      </div>
-    );
-  }
+const Header = ({currentPage, pageChangehandler}) => {
+  return(
+    <div className='d-flex container-fluid bg-secondary align-items-center py-2'>
+      <h1 className='text-light px-4 me-5 bg-secondary'>Sheri Foster</h1>
+
+      <ul className='nav navbar-custom justify-content-end'>
+        <li classname='nav-item nav-item-custom'><a href="About" onClick={() => pageChangeHandler('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</a></li>
+        <li classname='nav-item nav-item-custom'><a href="Portfolio" onClick={() => pageChangeHandler('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a></li>
+        <li classname='nav-item nav-item-custom'><a href="Contact" onClick={() => pageChangeHandler('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact Me</a></li>
+        <li classname='nav-item nav-item-custom'><a href={Resume} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a></li>
+
+      </ul>
+    </div>
+  )
+
+}
   
   export default Header;
