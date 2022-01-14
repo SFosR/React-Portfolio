b32a9ddaed6213e6cc0e5808dc774711bd2534af
Sheri Foster <fostr.sheri@gmail.com>
2022-01-14 13:23:49 -0500

    "some final changes"

--- src/component/pages/aboutMe.js
+++ src/component/pages/aboutMe.js
@@ -1,22 +1,30 @@
 import React from "react";
-// import MyPic from "../../Assets/sherifoster.jpg";
+import myPic from "../../Assets/selfpic.jpg";
+
+
 
 
 
 
 function AboutMe() {
     return (
-      <div className="about-me">
-        {/* <img src={MyPic} className="selfie"></img> */}
-      <div>
-        <p>I always thought I should be an engineer of some sort. In fact, every "career test" I've ever taken has said so.  
-        After years in sales, having success but not completely loving it, So I decided to actually do something about it. I enrolled in a coding bootcamp. I'm Full Stack web developer with skills in CSS, HTML, JavaScript, MySql, React with more to come. I recently earned a certificate in 
-      full stack develpment from THE Ohio State University. With a background in sales, I'm excited to bring my problem solving, independent and 
-      leadership skills to create meaningful web applicatons. I'm experienced in working in a fast-paced environment, working independently and also as a team. I'm a firm believer 
-      that in this ever changing world, you must constantly be learning. 
-      </p>
-      </div>
-      </div>
+
+    
+    <div className="about-me"> 
+       
+         <img src='../../Assets/selfpic.jpg' className='img-thumbnail shadow-4' alt='...' style={{ maxWidth: '24rem' }} />
+
+        <section>
+          <p>I always thought I should be an engineer of some sort. In fact, every "career test" I've ever taken has said so.  
+          After years in sales, having success but not completely loving it, So I decided to actually do something about it. I enrolled in a coding bootcamp. I'm Full Stack web developer with skills in CSS, HTML, JavaScript, MySql, React with more to come. I recently earned a certificate in 
+          full stack develpment from THE Ohio State University. With a background in sales, I'm excited to bring my problem solving, independent and 
+          leadership skills to create meaningful web applicatons. I'm experienced in working in a fast-paced environment, working independently and also as a team. I'm a firm believer 
+          that in this ever changing world, you must constantly be learning. 
+          </p>
+        </section> 
+
+    </div>  
+      
     );
   }
   
--- src/component/pages/portfolio.js
+++ src/component/pages/portfolio.js
@@ -10,9 +10,9 @@ export default function Portfolio() {
     return (
       <div className='container-fluid justify-content-center'>
 
-      <div className='row row-col-2 justify-content-center border bg-success py-2 align-items-start'>
+      <div className='row row-col-2 justify-content-center border bg-secondary py-2 align-items-start'>
         
-      <div className='col-md-4 col-md-2 text-center m-3 border app-bg'>
+      <div className='col-md-3 col-md-2 text-center m-2 border app-bg bg-success'>
         <h3 className='text-center'>Project Picker</h3>
         <p className='text-center'>Project Picker helps students find and share project ideas.</p>
         <img src={ProjectPicker} className="photos"></img>
@@ -20,7 +20,7 @@ export default function Portfolio() {
         <a className='btn btn-outline-dark mx-1 mb-3 bg-white' href="https://github.com/SFosR/Project-Picker.git" target='_blank'> GitHub</a>
         </div>  
 
-        <div className='col-md-4 col-md-2 text-center m-3 border app-bg'>
+        <div className='col-md-3 col-md-2 text-center m-2 border app-bg bg-success'>
         <h3 className='text-center'>Weather Dashboard</h3>
         <p className='text-center'>Project Picker helps students find and share project ideas.</p>
         <img src={WeatherDashboard} className="photos"></img>
@@ -28,7 +28,7 @@ export default function Portfolio() {
         <a className='btn btn-outline-dark mx-1 mb-3 bg-white' href="https://github.com/SFosR/Weather-Dash.git" target='_blank'> GitHub</a>
         </div>  
 
-        <div className='col-md-4 col-md-2 text-center m-3 border app-bg'>
+        <div className='col-md-4 col-md-2 text-center m-2 border app-bg bg-success'>
         <h3 className='text-center'>Note Taker</h3>
         <p className='text-center'>App that allows you to organize and save notes.</p>
         <img src={NoteTaker} className="photos"></img>
@@ -36,7 +36,7 @@ export default function Portfolio() {
         <a className='btn btn-outline-dark mx-1 mb-3 bg-white' href="https://github.com/SFosR/Note-Taker.git" target='_blank'> GitHub</a>
         </div>  
 
-        <div className='col-md-4 col-md-2 text-center m-3 border app-bg'>
+        <div className='col-md-4 col-md-2 text-center m-2 border app-bg bg-success'>
         <h3 className='text-center'>Employee Management System</h3>
         <p className='text-center'>System that allows employers to organize and manage employee information.</p>
         <img src={EmployeeMgmtSys} className="photos"></img>
@@ -44,7 +44,7 @@ export default function Portfolio() {
         <a className='btn btn-outline-dark mx-1 mb-3 bg-white' href="https://github.com/SFosR/Employee-Tracker.git" target='_blank'> GitHub</a>
         </div>  
 
-        <div className='col-md-4 col-md-2 text-center m-3 border app-bg'>
+        <div className='col-md-4 col-md-2 text-center m-2 border app-bg bg-success'>
         <h3 className='text-center'>Code Quiz</h3>
         <p className='text-center'>My first coding bootcamp project</p>
         <img src={CodeQuiz} className="photos"></img>
